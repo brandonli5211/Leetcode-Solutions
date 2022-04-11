dbaef21e187bddbc46ad951f913c86b3bc95c709
brandonli5211 <103465318+brandonli5211@users.noreply.github.com>
2022-04-11 19:04:34 -0400

    Update checkIfNAndItsDoubleExist.java

--- checkIfNAndItsDoubleExist.java
+++ checkIfNAndItsDoubleExist.java
@@ -1,28 +1,17 @@
 class Solution {
-  public boolean checkIfExist(int[] arr) {
+    public static boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < arr.length; i++){
+            if(set.contains((arr[i] * 2))){
+                return true;
+            }
 
-    /*
-    Runtime: 1 ms, faster than 98.62% of Java online submissions for Check If N and Its Double Exist.
-    Memory Usage: 41.2 MB, less than 5.35% of Java online submissions for Check If N and Its Double Exist.
-    Runtime: O(n)
-    */
-    
-    Set<Integer> nums = new HashSet<>(); // store arr[] elements here for fast lookup of doubles and halves
-
-    for (int i : arr) { // traverse the array
-
-      if (nums.contains(i * 2)) { // check whether i has a double
-        return true;
-      }
-
-      if (i % 2 == 0 && nums.contains(i / 2)) { // if i is even, check whether it has a half
-        return true;
-      }
-
-      nums.add(i); // if no matches to doubles or halves, just add to the set and continue for loop
-
+            if(arr[i] % 2 == 0 && set.contains(arr[i] / 2)){
+                return true;
+            }
+            
+            set.add(arr[i]);
+        }
+        return false;
     }
-
-    return false; // if we weren't able to return true from the for loop, return false
-  }
 }
